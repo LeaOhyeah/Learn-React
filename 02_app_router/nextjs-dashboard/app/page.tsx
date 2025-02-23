5a98909246bcd5ca3782d6282df62d1aa097daed
LeaOhyeah <leasehat@gmail.com>
2025-02-23 15:11:08 +0700

    Update page.tsx - test redeploy

--- 02_app_router/nextjs-dashboard/app/page.tsx
+++ 02_app_router/nextjs-dashboard/app/page.tsx
@@ -23,7 +23,8 @@ export default function Page() {
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , brought to you by Vercel. 
+            <strong> Developed by Lea Alyu 🚀</strong>
           </p>
           <Link
             href="/login"
